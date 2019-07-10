66ceea23e9795d8a4185a51f27db26991d19a92f
SEAN H F HE (ITA-ISDC-ISD-OOCLL/ZHA) <HESE2@OOCL.COM>
2019-07-10 17:26:03 +0800

    finish main.js some function

--- main/main.js
+++ main/main.js
@@ -1,3 +1,42 @@
 'use strict';
 
 //TODO: 请在该文件中实现练习要求并删除此注释
+function isContainItem(tags){
+    const allItems=loadAllItems();
+    let isContain=1;
+    tags.forEach(tag => {
+        let flag=0;
+        allItems.forEach(item=>{
+            if(tag.split("-")[0]===item.barcode) flag=1;
+        });
+        if(flag===0) isContain=0;
+    });
+    if(isContain===1) return true;
+    else return false;
+}
+
+function getItemList(tags){
+    const allItems=loadAllItems();
+    const itemList={};
+    tags.forEach(tag=>{
+        allItems.forEach(item=>{
+            if(tag.split("-")[0]===item.barcode){
+                if(tag.split("-")[1]!==null){
+                    itemList[item]=itemList[item]>=1?itemList[item]+tag.split("-")[1]:tag.split("-")[1];
+                }
+                else itemList[item]=itemList[item]>=1?itemList[item]+1:1;
+            }
+        });
+    });
+    return itemList;
+}
+function printReceipt(tags){
+    const itemList=getItemList(tags);
+    const promotions=loadPromotions()[0]['barcodes'];
+    for(let i in itemList){
+        for(let j in promotions){
+            if(i.barcode){}
+        }
+    }
+
+}
